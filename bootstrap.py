46b8c1ea92ac07c925f29c8c6ab6ca493a3cc29f
Igor Bondarenko <jetmind2@gmail.com>
2013-02-26 14:50:02 +0200

    Added zsh and some refactoring of bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -13,6 +13,10 @@ symlinks = [
     ('vim', '.vim'),
     ('vimrc', '.vimrc'),
 
+    # zsh
+    ('zsh/zshrc', '.zshrc'),
+    ('zsh/jetprose.zsh-theme', '.oh-my-zsh/themes/jetprose.zsh-theme'),
+
     # awesome wm
     ('awesome', '.config/awesome'),
     ('gtkrc-2.0', '.gtkrc-2.0'),
@@ -28,17 +32,19 @@ symlinks = [
 ]
 
 
+def run(command):
+    print ' '.join(command)
+    check_call(command)
+
+
 def install_zsh():
-    pass
+    oh_zsh_url = 'git://github.com/robbyrussell/oh-my-zsh.git'
+    run(['git', 'clone', oh_zsh_url, join(home, '.oh-my-zsh')])
 
 
 def bootstrap_vim():
-    command = ['git', 'submodule', 'init']
-    print ' '.join(command)
-    check_call(command)
-    command = ['git', 'submodule', 'update']
-    print ' '.join(command)
-    check_call(command)
+    run(['git', 'submodule', 'init'])
+    run(['git', 'submodule', 'update'])
     # TODO: install Command-T
 
 
@@ -56,13 +62,9 @@ def bootstrap():
         if exists(path):
             print path, 'exists, backing up'
             suffix = '.backup-%s' % datetime.now().strftime('%Y%m%d')
-            command = ['mv', path, path + suffix]
-            print ' '.join(command)
-            check_call(command)
+            run(['mv', path, path + suffix])
 
-        command = ['ln', '-s', target, path]
-        print ' '.join(command)
-        check_call(command)
+        run(['ln', '-s', target, path])
     for f in after:
         f()
 
