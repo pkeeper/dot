563a56674545946c4e6e87906f6c9995f2599d09
Igor Bondarenko <jetmind2@gmail.com>
2013-02-26 14:02:00 +0200

    Added bootstrap script, readme, and gitignore

---
+++ README.md
@@ -0,0 +1,12 @@
+INSTALLATION
+============
+
+  $ git clone ... ~/dot
+  $ cd ~/dot
+  $ python bootstrap.py
+
+ADDING NEW CONFIGS
+==================
+
+* Copy config/folder into repo.
+* Edit `bootstrap.py`. Add paths for new config into `symlinks` list.
---
+++ bootstrap.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from os.path import join, normpath, exists
+from datetime import datetime
+from subprocess import check_call
+
+home = os.path.expanduser('~')
+dot_base = os.getcwd()
+
+# (target, symlink)
+symlinks = [
+    # awesome wm
+    ('awesome', '.config/awesome'),
+    ('gtkrc-2.0', '.gtkrc-2.0'),
+    ('gtkrc.mine', '.gtkrc.mine'),
+]
+symlinks = [
+    (normpath(join(dot_base, tp)), normpath(join(home, sp)))
+    for tp, sp in symlinks
+]
+
+
+def bootstrap():
+    for target, path in symlinks:
+        # check that symlink path does not exists
+        if exists(path):
+            print path, 'exists, backing up'
+            suffix = '.backup-%s' % datetime.now().strftime('%Y%m%d')
+            command = ['mv', path, path + suffix]
+            print ' '.join(command)
+            check_call(command)
+
+        command = ['ln', '-s', target, path]
+        print ' '.join(command)
+        check_call(command)
+    # TODO: bootstrap vim (plugins as submodules, install Commant-T)
+
+if __name__ == "__main__":
+    bootstrap()
